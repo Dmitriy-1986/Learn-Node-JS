607e48db5a38abe9fc6ed6e38dba7503dc800c0c
Dovgal Dima <52016913+Dmitriy-1986@users.noreply.github.com>
2022-06-12 19:27:21 +0300

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,8 +1,15 @@
-var http = require('http');
+var http = require('http'); //require - встроеная функция для включения внешних модулей в отдельных файлах
 
-http.createServer((request, response) => {
-  response.writeHead(200, {'Content-type': 'text/plain'});
-  response.end('Hello World\n');
+http.createServer((request, response) => { //createServer -  превращает компьютер в http сервер
+  response.writeHead(200, {'Content-type': 'text/plain'}); //writeHead - встроеное свойство http которое отправляет заголовок ответа на запрос, добавляет в ответ статусный код и набор заголовков
+  response.end('Hello World\n'); //end - сигнализирует серверу, что заголовки и тело ответа установлены, в итоге ответ отсылается клиента. Данный метод должен вызываться в каждом запросе.
 }).listen(8080);
 
 console.log('Server running at port 8080');
+
+
+//write: пишет в поток ответа некоторое содержимое
+//setHeader(name, value): добавляет в ответ один заголовок
+//statusMessage: устанавливает сообщение, отправляемое вместе со статусным кодом
+//statusCode: устанавливает статусный код ответа
+//метод listen()  чтобы сервер мог прослушивать и обрабатывать входящие подключения, в который в качестве параметра передается номер порта, по которому запускается сервер.
